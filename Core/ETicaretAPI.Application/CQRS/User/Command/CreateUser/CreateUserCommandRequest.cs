0d11ee6103b2c8e1299eded9fe11c0bf3da7758a
Koray Karakoyun <koraykarakoyun@hotmail.com>
2022-09-13 20:20:29 +0300

    createuser controllerı ve cqrs pattern uygulandi

---
+++ Core/ETicaretAPI.Application/CQRS/User/Command/CreateUser/CreateUserCommandRequest.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.CQRS.User.Command.CreateUser
+{
+    public class CreateUserCommandRequest:IRequest<CreateUserCommandResponse>
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordConfirm { get; set; }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using ETicaretAPI.Application.CQRS.User.Command.CreateUser;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        IMediator _mediator;
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> CreateUser(CreateUserCommandRequest createUserCommandRequest)
+        {
+
+            CreateUserCommandResponse createUserCommandResponse = await _mediator.Send(createUserCommandRequest);
+            return Ok(createUserCommandResponse);
+
+
+
+        }
+
+
+
+    }
+}
